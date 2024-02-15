389a3e181f513b14e20b08315971f9de634e174f
Shade-v2006 <Shade-v2006@yandex.ru>
2024-02-16 05:04:03 +0700

    Task1 has been solved

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*    Задача 1: Задайте значения M и N. Напишите программу, 
+    которая выведет все натуральные числа в промежутке 
+    от M до N. Использовать рекурсию, не использовать циклы.
+*/
+
+void recursia(int m, int n)
+{
+    if (m <= n)
+    {
+        System.Console.Write(value: $"{m} ");
+        recursia(m: m + 1, n);
+    }
+}
+System.Console.WriteLine(value: $"Введите первое число:");
+int m = Convert.ToInt32(value: Console.ReadLine()!);
+System.Console.WriteLine(value: $"Введите второе число больше первого:");
+int n = Convert.ToInt32(value: Console.ReadLine()!);
+recursia(m, n);
